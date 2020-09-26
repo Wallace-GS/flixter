4a4bb150447d0f774f9040a1817c4ccb0ff7f19f
Wallace-GS <wgsantos29@gmail.com>
2020-09-26 16:59:59 -0700

    finishes flixter p2 required user stories

--- app/src/main/java/com/codepath/wallacegs/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/codepath/wallacegs/flixster/adapters/MovieAdapter.java
@@ -1,19 +1,24 @@
 package com.codepath.wallacegs.flixster.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.codepath.wallacegs.flixster.DetailActivity;
 import com.codepath.wallacegs.flixster.R;
 import com.codepath.wallacegs.flixster.models.Movie;
+
+import org.parceler.Parcels;
 
 import java.util.List;
 
@@ -47,22 +52,24 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        RelativeLayout container;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
-            tvTitle = itemView.findViewById(R.id.tvTitle);
-            tvOverview = itemView.findViewById(R.id.tvOverview);
-            ivPoster = itemView.findViewById(R.id.ivPoster);
+            this.tvTitle = itemView.findViewById(R.id.tvTitle);
+            this.tvOverview = itemView.findViewById(R.id.tvOverview);
+            this.ivPoster = itemView.findViewById(R.id.ivPoster);
+            this.container = itemView.findViewById(R.id.container);
         }
 
-        public void bind(Movie movie) {
+        public void bind(final Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText((movie.getOverview()));
             String imageUrl;
-            
+
             if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
                 imageUrl = movie.getBackdropPath();
             } else {
@@ -70,6 +77,14 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             }
 
             Glide.with(context).load(imageUrl).into(ivPoster);
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movie));
+                    context.startActivity(i);
+                }
+            });
         }
     }
 }
--- app/src/main/java/com/codepath/wallacegs/flixster/models/Movie.java
+++ app/src/main/java/com/codepath/wallacegs/flixster/models/Movie.java
@@ -3,21 +3,31 @@ package com.codepath.wallacegs.flixster.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    float rating;
+
+    // empty constructor needed by Parceler lib
+    public Movie() {}
 
     public Movie(JSONObject jsonObject) throws JSONException {
         this.backdropPath = jsonObject.getString("backdrop_path");
         this.posterPath = jsonObject.getString("poster_path");
         this.title = jsonObject.getString("title");
         this.overview = jsonObject.getString("overview");
+        this.rating = (float) jsonObject.getDouble("vote_average");
+        this.movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -42,5 +52,13 @@ public class Movie {
 
     public String getOverview() {
         return overview;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
